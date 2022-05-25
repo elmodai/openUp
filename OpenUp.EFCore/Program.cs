b5595a7f7be63a9c4707ead1781b9ed60914f068
aarune <ayod.arune@cedsif.gov.mz>
2022-05-25 13:50:35 +0200

    Update

--- OpenUp.EFCore/Program.cs
+++ OpenUp.EFCore/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using OpenUp.EFCore.Autores;
+using OpenUp.EFCore.EFContext;
+using OpenUp.EFCore.Livros;
+
+
+
+LivroContext ctx = new();
+
+ctx.Add(
+    new Livro
+    {
+        Autor = new Autor
+        {
+            Nome = "Ele",
+            WebUrl = "www.blogdele.com"            
+        },
+
+        Titulo = "Aquele livro"
+    }
+);
+
+//ctx.SaveChanges();
+
+Console.WriteLine($"{ctx.Livros.FirstOrDefault().Titulo}");
